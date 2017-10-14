411851f816771b53334a574ae9a3f47278fe5b19
andres <andres@shuttlecloud.com>
2017-10-14 15:40:28 +0200

    minor configuration of vue and browserify

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 /* globals Vue */
 "use strict";
 
-const ipcRenderer = require('electron').ipcRenderer;
+// const ipcRenderer = require('electron').ipcRenderer;
 
+<<<<<<< HEAD
 const TasksHandler = require('./app/renderer/tasks_handler');
 const AppStatus = require('./app/renderer/app_status');
 const Material = require('./app/renderer/materialize');
+=======
+// const TaskConfig = require('./app/renderer/task_config');
+// const AppStatus = require('./app/renderer/app_status');
+// const Material = require('./app/renderer/materialize');
+//
+// const components = {
+//     "task-suite": require('./app/renderer/components/task_suite'),
+//     "navbar": require('./app/renderer/components/navbar'),
+//     "bottom-bar": require('./app/renderer/components/bottom_bar'),
+//     "config-menu": require('./app/renderer/components/config_menu')
+// };
+>>>>>>> minor configuration of vue and browserify
 
-const components = {
-    "task-suite": require('./app/renderer/components/task_suite'),
-    "navbar": require('./app/renderer/components/navbar'),
-    "bottom-bar": require('./app/renderer/components/bottom_bar'),
-    "config-menu": require('./app/renderer/components/config_menu')
-};
 
+<<<<<<< HEAD
 
 ipcRenderer.on('before-close', () => {
     TasksHandler.saveTasks();
@@ -22,10 +30,20 @@ ipcRenderer.on('before-close', () => {
         ipcRenderer.send("close-app");
     });
 });
+=======
+// ipcRenderer.on('before-close', () => {
+//     TaskConfig.saveTasks();
+//     const promises = TaskConfig.suites.map((s) => s.stopAll());
+//     Promise.all(promises).then(() => {
+//         ipcRenderer.send("close-app");
+//     });
+// });
+>>>>>>> minor configuration of vue and browserify
 
 const app = new Vue({ // eslint-disable-line no-unused-vars
     el: '#app',
     data: {
+<<<<<<< HEAD
         suites: TasksHandler.suites,
         AppStatus: AppStatus
     },
@@ -35,10 +53,22 @@ const app = new Vue({ // eslint-disable-line no-unused-vars
     },
     mounted() {
         Material.init();
+=======
+        suites: [],
+        loaded: false,
+        // AppStatus: AppStatus
+    },
+    // components: components,
+    mounted() {
+        // Material.init();
+        // TaskConfig.loadTasks();
+        // this.suites = TaskConfig.suites;
+        // this.loaded = true;
+>>>>>>> minor configuration of vue and browserify
     },
     updated() {
-        this.$nextTick(() => {
-            Material.init();
-        });
+        // this.$nextTick(() => {
+        //     Material.init();
+        // });
     }
 });
